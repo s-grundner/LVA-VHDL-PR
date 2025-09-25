1354d6acc83b3e14004910556d1836f7b94d2141
s-grundner <simon.grundner@gmail.com>
2025-09-26 00:58:24 +0200

    feat: ✨ Reworked EX02

---
+++ EX02/counter_e.vhd
@@ -0,0 +1,17 @@
+--------------------------------------------------------------------------------
+-- file: counter_e.vhd
+-- type: Entity
+-- author: Simon Grundner (k12136610)
+-- brief: Counter module declaration
+--------------------------------------------------------------------------------
+
+-- ENTITY
+
+entity counter is
+    port (
+        clk_i 						       : in bit;
+        rst_i 						       : in bit;
+        counter_restart_strobe_i : in bit;
+        counter_value_o 			   : out natural
+    );
+end entity counter;
